3acad856b2d649aa8311aa0c514ee297383b5d34
SImon <33484974+simonjuha@users.noreply.github.com>
2023-12-17 22:48:12 +0100

    added main code

---
+++ include/ringbuffer.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <stdint.h>
+
+/*
+    The size of the buffer will affect the latency.
+        buffer size 16 : 1ms
+        buffer size 32 : 1.7ms
+        buffer size 64 : 3.3ms
+        buffer size 128: 6.5ms
+    Buffer size larger than 128 might cause problems with RAM overflow.
+*/
+#define BUFFER_SIZE 16
+
+volatile uint16_t ringBuffer[BUFFER_SIZE];
+volatile uint8_t  bufferCount = 0;
+volatile uint8_t  bufferReadIndex = 0;
+volatile uint8_t  bufferWriteIndex = 0;
+
+void writeToBuffer(uint16_t value) {
+    if(bufferCount >= BUFFER_SIZE) return; // buffer full
+    ringBuffer[bufferWriteIndex] = value;
+    bufferWriteIndex = (bufferWriteIndex + 1) % BUFFER_SIZE; // wrap around
+    bufferCount++;
+}
+
+uint16_t readFromBuffer() {
+    while(bufferCount <= 0) {}
+    uint16_t value = ringBuffer[bufferReadIndex];
+    bufferReadIndex = (bufferReadIndex + 1) % BUFFER_SIZE; // wrap around
+    bufferCount--;
+    return value;
+}
