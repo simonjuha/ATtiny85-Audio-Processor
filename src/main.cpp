a60d81fa7d27d8ae52381598ac4b110b43034f76
SImon <33484974+simonjuha@users.noreply.github.com>
2023-12-18 17:02:59 +0100

    improved sample rate

--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,36 @@
 
 // main loop
 int main() {
-    /* -------- PWM setup -------- */
-    DDRB |= 1<<PB1;                 // Enable PWM output pins
-    PLLCSR = 1<<PCKE | 1<<PLLE;     // Enable PLL
+    /* -------- PWM setup (Timer1) -------- */
+    DDRB |= 1<<PB1;                         // Enable PWM output pins
+    PLLCSR = 1<<PCKE | 1<<PLLE;             // Enable PLL
     TCCR1 = 1<<PWM1A | 2<<COM1A0 | 1<<CS10; // PWM A, clear on match, 1:1 prescale
 
-    /* -------- Timer setup -------- */
+    /* -------- Timer setup (Timer0) -------- */
+    // Output sample rate formula: F_CPU / (prescale * (1 + OCR0A))
+    // 8 MHz / ( 1 * (1 * 24) ) = 320 kHz
     TCCR0A = 3<<WGM00;              // Fast PWM
-    TCCR0B = 1<<WGM02 | 2<<CS00;    // 1/8 prescale
+    TCCR0B = 1<<WGM02;              // Fast PWM
+    // CS 0:2 bits in TCCR0B sets the prescale
+    // 0b000: no clock source, 0b001: no prescaler, 0b010: 8, 0b011: 64, 0b100: 256, 0b101: 1024 
+    TCCR0B |= 2<<CS00;              // no prescaler
     TIMSK = 1<<OCIE0A;              // Enable compare match, disable overflow
-    OCR0A = 49;
+    OCR0A = 24;                     // Compare value
 
     /* -------- ADC setup -------- */
+    // Input sample rate formula: F_CPU / (prescale * (1 + (ADCSRA & 0x07)))
+    // 8 MHz / (8 * 3) = 333.333 kHz
     DDRB &= ~(1<<PB4);              // Enable ADC input on pin 3 (A2/PB4)
     ADMUX = 0b00000010;             // Use ADC2
-    ADCSRA = 1<<ADEN | 1<<ADATE | 1<<ADPS2 | 1<<ADPS1;
-    ADCSRB = 0x40;                  // Enable free running mode
-    DIDR0 = 0x04;                   // Disable digital input on ADC2
+    ADCSRA = 1<<ADEN;               // Enable ADC
+    ADCSRA |= 1<<ADATE;             // Enable auto trigger
+    // ADPS 0:2 bits in ADCSRA sets the prescale
+    // 0b000: 2, 0b001: 2, 0b010: 4, 0b011: 8, 0b100: 16, 0b101: 32, 0b110: 64, 0b111: 128
+    ADCSRB |= 1<<ADPS1 | 1<<ADPS0;  // Set prescale to 8
+    ADCSRB = 1<<ACME;               // Enable analog comparator multiplexer
+    DIDR0 |= 1<<ADC1D;              // Disable digital input on ADC2
     ADCSRA |= 1<<ADSC;              // Start conversion
+    
 
     sei(); // Enable interrupts
 
@@ -54,7 +66,7 @@ int main() {
     return 0;
 }
 
-// sample rate = 8 MHz / 8 / 50 = 20 kHz
+// sample rate = 8 MHz / (1 * (1 + 24)) = 320 kHz
 ISR(TIMER0_COMPA_vect) {
     #if PASSTHROUGH
         uint16_t value = ADC >> 2;
